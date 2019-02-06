9308f82d30501b98a69aa6c70ea35cab8e22656a
dghadge <nyy021@gmail.com>
2019-02-06 11:46:46 -0500

    changed ip

--- scripts/psn-migration.ps1
+++ scripts/psn-migration.ps1
@@ -44,7 +44,7 @@ switch ($optionSelected) {
 
             #replace SQL server IP address
             Write-Host "Replacing SQLServer IP address on WebServer"
-            replace-hostname $searchDir $searchPattern 10.178.4.57 99.999.9.99
+            replace-hostname $searchDir $searchPattern XX.XXX.4.57 99.999.9.99
 
             #replace file server name
             Write-Host "Replacing FileShare name on WebServer"
@@ -57,76 +57,76 @@ switch ($optionSelected) {
 
             #replace SQL Server IP address
             Write-Host "Replacing SQLServer IP address on JavaServer"
-            replace-hostname c:\JBM\webserver\db.conf $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname c:\JBM\webserver\glassfish4\glassfish\domains\IISDomain\config\domain.xml $searchPattern 10.178.4.57 99.999.9.99
+            replace-hostname c:\JBM\webserver\db.conf $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname c:\JBM\webserver\glassfish4\glassfish\domains\IISDomain\config\domain.xml $searchPattern XX.XXX.4.57 99.999.9.99
         }
     3   { 
             #----- Start search/replace for UEFIPSNDEVDT01 ----#
             Write-Host "Replacing SQLServer IP address on hostname "$env:COMPUTERNAME
-            replace-hostname C:\bill\web_production\vendors\datadeals\datadealsproduction.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\dailystylestimer\dailystylestimer.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\dailystylestimer\dailytopgunsassetflowupdate.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\dailystylestimer\stylesproduction.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\checkproduction.xml $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\lipper\lipperconv.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\MPSN_CWOW\Webproduction.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\russelluniverse\russellUniverse.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\sbmax\sbconversion.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\styles\stylesproduction.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\zephyrpsn\zephyrpsn.ini $searchPattern 10.178.4.57 99.999.9.99
+            replace-hostname C:\bill\web_production\vendors\datadeals\datadealsproduction.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\dailystylestimer\dailystylestimer.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\dailystylestimer\dailytopgunsassetflowupdate.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\dailystylestimer\stylesproduction.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\checkproduction.xml $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\lipper\lipperconv.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\MPSN_CWOW\Webproduction.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\russelluniverse\russellUniverse.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\sbmax\sbconversion.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\styles\stylesproduction.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\zephyrpsn\zephyrpsn.ini $searchPattern XX.XXX.4.57 99.999.9.99
         }
     4   { 
             #----- Start search/replace for UEFIPSNDEVDT02 ----#
             Write-Host "Replacing SQLServer IP address on hostname "$env:COMPUTERNAME
-            replace-hostname C:\MPSN_CWOW\MSSmithBarney.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\MPSN_CWOW\Webproduction.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\MPSN_CWOW\Dailyproduction.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\MPSN_CWOW\BarclayProduction.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\GlobalPSNUniverses\GlobalPSNUniverses.ini $searchPattern 10.178.4.57 99.999.9.99
+            replace-hostname C:\MPSN_CWOW\MSSmithBarney.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\MPSN_CWOW\Webproduction.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\MPSN_CWOW\Dailyproduction.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\MPSN_CWOW\BarclayProduction.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\GlobalPSNUniverses\GlobalPSNUniverses.ini $searchPattern XX.XXX.4.57 99.999.9.99
         }
     5   { 
             #----- Start search/replace for UEFIPSNDEVDT03 ----#
             Write-Host "Replacing SQLServer IP address on hostname "$env:COMPUTERNAME
-            replace-hostname C:\MPSN_CWOW\MSSmithBarney.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\MPSN_CWOW\Webproduction.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\MPSN_CWOW\Dailyproduction.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\MPSN_CWOW\BarclayProduction.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\GlobalPSNUniverses\GlobalPSNUniverses.ini $searchPattern 10.178.4.57 99.999.9.99
+            replace-hostname C:\MPSN_CWOW\MSSmithBarney.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\MPSN_CWOW\Webproduction.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\MPSN_CWOW\Dailyproduction.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\MPSN_CWOW\BarclayProduction.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\GlobalPSNUniverses\GlobalPSNUniverses.ini $searchPattern XX.XXX.4.57 99.999.9.99
         }
     6   { 
             #----- Start search/replace for UEFIPSNDEVDT04 ----#
             Write-Host "Replacing SQLServer IP address on hostname "$env:COMPUTERNAME
-            replace-hostname C:\MPSN_CWOW\Webproduction.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\MPSN_CWOW\Dailyproduction.ini $searchPattern 10.178.4.57 99.999.9.99
+            replace-hostname C:\MPSN_CWOW\Webproduction.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\MPSN_CWOW\Dailyproduction.ini $searchPattern XX.XXX.4.57 99.999.9.99
         }
     7   { 
             #----- Start search/replace for UEFIPSNDEVDT05 ----#
             Write-Host "Replacing SQLServer IP address on hostname "$env:COMPUTERNAME
-            replace-hostname C:\MPSN_CWOW\Webproduction.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\MPSN_CWOW\Dailyproduction.ini $searchPattern 10.178.4.57 99.999.9.99
+            replace-hostname C:\MPSN_CWOW\Webproduction.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\MPSN_CWOW\Dailyproduction.ini $searchPattern XX.XXX.4.57 99.999.9.99
         }
     8   { 
             #----- Start search/replace for UEFIPSNDEVDT06 ----#
             Write-Host "Replacing SQLServer IP address on hostname "$env:COMPUTERNAME
-            replace-hostname C:\MPSN_CWOW\Webproduction.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\MPSN_CWOW\Dailyproduction.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\MPSN_CWOW\BarclayProduction.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\GlobalPSNUniverses\GlobalPSNUniverses.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\bill\web_production\daily_update.ini $searchPattern 10.178.4.57 99.999.9.99
+            replace-hostname C:\MPSN_CWOW\Webproduction.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\MPSN_CWOW\Dailyproduction.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\MPSN_CWOW\BarclayProduction.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\GlobalPSNUniverses\GlobalPSNUniverses.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\bill\web_production\daily_update.ini $searchPattern XX.XXX.4.57 99.999.9.99
         }
     9   { 
             #----- Start search/replace for UEFIPSNDEVDF01 ----#
             Write-Host "Replacing SQLServer IP address on hostname "$env:COMPUTERNAME
-            replace-hostname D:\checkproduction\checkproduction.xml $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\bill\merrill_lynch_daily\mluniverse.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname D:\bill\web_production\daily_update.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname D:\bill\web_production\web_production.ini $searchPattern 10.178.4.57 99.999.9.99
+            replace-hostname D:\checkproduction\checkproduction.xml $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\bill\merrill_lynch_daily\mluniverse.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname D:\bill\web_production\daily_update.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname D:\bill\web_production\web_production.ini $searchPattern XX.XXX.4.57 99.999.9.99
         }
     10  {
             #----- Start search/replace for UEFIPSNDEVECM1 ----#
             Write-Host "Replacing SQLServer IP address on hostname "$env:COMPUTERNAME
-            replace-hostname C:\newmgr\newmanagerReport2013.ini $searchPattern 10.178.4.57 99.999.9.99
-            replace-hostname C:\uploadtoecms\uploadtoecms.xml $searchPattern 10.178.4.57 99.999.9.99
+            replace-hostname C:\newmgr\newmanagerReport2013.ini $searchPattern XX.XXX.4.57 99.999.9.99
+            replace-hostname C:\uploadtoecms\uploadtoecms.xml $searchPattern XX.XXX.4.57 99.999.9.99
         }
 }
 Write-Host 'Completed....'
